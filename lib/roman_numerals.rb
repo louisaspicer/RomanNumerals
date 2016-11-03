404575810d7be0a8c9d003ae5e27caf021e5033f
Louisa <louisaspicer@gmail.com>
2016-11-03 14:55:53 +0000

    created first Rspec tests

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,7 @@
+class RomanNumerals
+    ROMAN_NUMERALS = {10 => "X", 5 => "V", 1 => "I"}
+
+  def self.convert(number)
+    ROMAN_NUMERALS[number]
+  end
+end
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,15 @@
+require './lib/roman_numerals.rb'
+
+describe RomanNumerals do
+  it "should be able to convert 1 to I" do
+    expect(RomanNumerals.convert(1)).to eq("I")
+  end
+
+  it "should be able to convert 5 to V" do
+    expect(RomanNumerals.convert(5)).to eq("V")
+  end
+
+  it "should be able to covert 10 to X" do
+    expect(RomanNumerals.convert(10)).to eq("X")
+  end
+end

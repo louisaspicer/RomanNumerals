f54b36403193180e254316477cb3b9120ec94b52
Louisa <louisaspicer@gmail.com>
2016-11-03 15:41:49 +0000

    passes tests for any number

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,7 +1,19 @@
 class RomanNumerals
-    ROMAN_NUMERALS = {10 => "X", 5 => "V", 1 => "I"}
+    ROMAN_NUMERALS = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
+                    100 => "C", 90 => "XC", 50 => "L", 40 => "XL",
+                    10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
 
   def self.convert(number)
-    ROMAN_NUMERALS[number]
+    roman_numeral = ""
+    ROMAN_NUMERALS.each do |k, v|
+      #for each key, starting at the first one, input the value
+      #that many times. e.g. number = 250
+      #once the loop reaches 100, it will do 250/100 = 2, adding "C" 2 times.
+      #then each time it subtracts 100(the key) off of the 'number'
+      #then it will be left with 50, so when it gets to the 50 key,
+      #it can only do it 1 time, so will add "L" to the end once.
+      (number / k).times {roman_numeral << v; number = number - k}
+    end
+    roman_numeral
   end
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -12,4 +12,12 @@ describe RomanNumerals do
   it "should be able to covert 10 to X" do
     expect(RomanNumerals.convert(10)).to eq("X")
   end
+
+  it "should be able to convert 35 to XXXV" do
+    expect(RomanNumerals.convert(35)).to eq("XXXV")
+  end
+
+  it "should be able to convert 996 to CMIXVI" do
+    expect(RomanNumerals.convert(996)).to eq("CMXCVI")
+  end
 end
